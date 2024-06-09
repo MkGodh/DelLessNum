9c8ff5bd331085fd5758e8e68e517d81261ccf2c
MkaGodh <fenryrvla@yandex.ru>
2024-06-09 11:13:11 +0300

    Добавьте файлы проекта.

---
+++ DelLessNum/DelLessNum.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int rNum;
+    int position = 1;
+
+    vector<int> vec;
+
+    while (true) {
+        cout << "Welcome to robot queue Input your id: ";
+        -cin >> rNum;
+
+        if (rNum == -1) {
+            break;
+        }
+
+        while (!vec.empty() && vec.back() < rNum) {
+            vec.pop_back(); // удаляем робота, чей номер меньше, чем у нового
+        }
+
+        vec.push_back(rNum); // добавляем нового робота в очередь
+    }
+
+    for (int i = 0; i < vec.size(); i++) {
+        cout << "Robot num: " << vec[i] << " position in queque: " << position << "\n";
+        position++;
+    }
+
+
+}
